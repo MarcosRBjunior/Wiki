901de1775a06d55b2aa30eb1295af0c85c4883ea
Marcos Ribeiro Junior <marcosribeirojr@alunos.fho.edu.br>
2026-08-26 12:19:01 -0300

    feat(api): define schema graphql e configura apollo server (#5)

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import 'dotenv/config';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './graphql/typeDefs.js';
+import { resolvers } from './graphql/resolvers.js';
+import { seedSeNecessario } from './data/personagensStore.js';
 
 const PORT = process.env.PORT || 4000;
 
-console.log(`API placeholder pronta. Servidor Apollo será configurado na task 5 (porta alvo: ${PORT}).`);
+await seedSeNecessario();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: PORT },
+});
+
+console.log(`Apollo Server pronto em ${url}`);

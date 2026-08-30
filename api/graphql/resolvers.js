a4e99b70873bc3e456383e941ed71f2db7361b18
Marcos Ribeiro Junior <marcosribeirojr@alunos.fho.edu.br>
2026-08-30 17:30:49 -0300

    feat(api): cria query personagem(id) para detalhe
    Adiciona a query personagem(id: ID!): Personagem ao schema GraphQL, reaproveitando buscarPersonagemPorId. Inclui cobertura de testes com node:test.

--- api/graphql/resolvers.js
+++ api/graphql/resolvers.js
@@ -1,8 +1,9 @@
-import { listarPersonagens } from '../data/personagensStore.js';
+import { listarPersonagens, buscarPersonagemPorId } from '../data/personagensStore.js';
 
 export const resolvers = {
   Query: {
     status: () => 'ok',
     personagens: () => listarPersonagens(),
+    personagem: (_, { id }) => buscarPersonagemPorId(id),
   },
 };
